d3c5f7b83b5b86a51862a7b9bb286ec1434f3f3d
Eun <dyd5361@gmail.com>
2024-08-15 00:29:08 -0400

    확인

---
+++ lesson-02.js
@@ -0,0 +1,10 @@
+let number1 = prompt('첫번째 숫자');
+let number2 = prompt('두번째 숫자');
+
+number1 = parseFloat(number1);
+number2 = parseFloat(number2);
+
+console.log('더하기:' + (number1 + number2));
+console.log('빼기:' + (number1 - number2));
+console.log('곱하기:' + (number1 * number2));
+console.log('나누기:' + (number1 / number2));
